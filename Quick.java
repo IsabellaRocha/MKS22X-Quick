987e3e0450e3e6b76abf40940e6e9e3df0a99be8
Isabella Rocha <irocha00@stuy.edu>
2019-03-13 19:54:51 -0400

    Optimized partition

--- Quick.java
+++ Quick.java
@@ -2,7 +2,10 @@ import java.util.*;
 public class Quick {
   public static int partition (int[] data, int start, int end) {
     int idx = 0;
-    int mid = ((end - start) / 2) + start;
+    int mid = 0;
+    int rand = (int) (Math.random() * 2);
+    if (rand == 0) mid = ((end - start) / 2) + start;
+    if (rand == 1) mid = ((end - start) / 2) + start + 1;
     if ((data[start] <= data[mid] && data[mid] <= data[end]) ||
         (data[end] <= data[mid] && data[mid] <= data[start])) idx = mid;
     if ((data[end] < data[start] && data[start] < data[mid]) ||
@@ -14,7 +17,7 @@ public class Quick {
     data[0] = pivot;
     while (start < end) {
       start++;
-      if (data[start] > pivot) {
+      if (data[start] > pivot && start < end) {
         while (data[end] > pivot && end > start) {
           end--;
         }
@@ -37,13 +40,11 @@ public class Quick {
       data[0] = data[start];
       data[start] = pivot;
     }
-    else {
+    else if (data[start] > pivot){
       data[0] = data[start - 1];
       data[start - 1] = pivot;
       start -= 1;
     }
-//    System.out.println(pivot);
-//    System.out.println(Arrays.toString(data));
     return start;
   }
   public static int quickselect(int[] data, int k) {
@@ -61,11 +62,23 @@ public class Quick {
     }
     return data[compare];
   }
+  public static int findMedian(int[] data, int start, int end) {
+    int idx = 0;
+    int mid = ((end - start) / 2) + start;
+    if ((data[start] <= data[mid] && data[mid] <= data[end]) ||
+        (data[end] <= data[mid] && data[mid] <= data[start])) idx = mid;
+    if ((data[end] < data[start] && data[start] < data[mid]) ||
+        (data[mid] < data[start] && data[start] < data[end])) idx = start;
+    if ((data[mid] < data[end] && data[end] < data[start]) ||
+        (data[start] < data[end] && data[end] < data[mid])) idx = end;
+        return data[idx];
+  }
   public static void main(String[] args) {
     int[] ary = {2, 10, 15, 23, 0,  5};
     int[] ary2 = {999,999,999,4,1,0,3,2,999,999,999};
     int[] ary3 = {17,61,67,47,93,12,20,4,44,78};
     int[] ary4 = {1, 2, 3, 4, 5, 6, 7, 8};
+//    System.out.println(findMedian(ary3, 4, 5));
 //    System.out.println(partition(ary3, 0, ary3.length - 1));
 //    System.out.println(partition(ary, 0, ary.length - 1));
     System.out.println(quickselect(ary, 0));
