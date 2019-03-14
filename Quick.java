63520b571c062995f88bb7c769eec35cc8363006
Isabella Rocha <irocha00@stuy.edu>
2019-03-14 00:04:02 -0400

    Tried changing partition

--- Quick.java
+++ Quick.java
@@ -1,43 +1,32 @@
 import java.util.*;
 public class Quick {
   public static int partition (int[] data, int start, int end) {
-    int idx = findMedian(data, start, end);
-    int pivot = data[idx];
-    data[idx] = data[0];
-    data[0] = pivot;
-    while (start < end) {
-      start++;
-      if (data[start] > pivot) {
-        if (data[end] < pivot) {
-          int s = data[start];
-          data[start] = data[end];
-          data[end] = s;
-          end--;
-        }
+    if (start == end) return start;
+    Random rand = new Random();
+    int pivot = findMedian(data, start, end);
+    swap(data, pivot, start);
+    start++;
+    while (start != end) {
+      int check = -1;
+      if (data[start] == data[0]) {
+        check = rand.nextInt(2);
       }
-      if (data[start] == pivot) {
-        int rando = (int) (Math.random() * 2);
-        if (rando == 0) {
-          int s = data[start];
-          int e = data[end];
-          data[start] = e;
-          data[end] = s;
-        }
+      if (data[start] < data[0] || check == 0) {
+        start++;
+      }
+      else if (data[start] > data[0] || check == 1) {
+        swap(data, start, end);
+        end--;
       }
     }
-    if (data[start] < pivot) {
-      data[0] = data[start];
-      data[start] = pivot;
+    if (data[start] < data[0]) {
+      swap(data, start, 0);
+      return start;
     }
-    else if (data[start] > pivot){
-      data[0] = data[start - 1];
-      data[start - 1] = pivot;
-      start -= 1;
+    else {
+      swap(data, start - 1, 0);
+      return start - 1;
     }
-//    System.out.println(start);
-//    System.out.println(pivot);
-//    System.out.println(Arrays.toString(data));
-    return start;
   }
   public static int quickselect(int[] data, int k) {
     int start = 0;
@@ -45,63 +34,66 @@ public class Quick {
     int compare = partition(data, start, end);
     while (compare != k) {
       if (compare < k) {
-        start = compare;
+        start = compare + 1;
       }
       if (compare > k) {
-        end = compare;
+        end = compare - 1;
       }
       compare = partition(data, start, end);
     }
     return data[compare];
   }
-  public static int findMedian(int[] data, int start, int end) {
+  private static int findMedian(int[] data, int start, int end) {
     int idx = 0;
-    int mid = ((end - start) / 2) + start;
+    int mid = ((end + start) / 2);
     if ((data[start] <= data[mid] && data[mid] <= data[end]) ||
         (data[end] <= data[mid] && data[mid] <= data[start])) idx = mid;
-    if ((data[end] < data[start] && data[start] < data[mid]) ||
+    else if ((data[end] < data[start] && data[start] < data[mid]) ||
         (data[mid] < data[start] && data[start] < data[end])) idx = start;
-    if ((data[mid] < data[end] && data[end] < data[start]) ||
-        (data[start] < data[end] && data[end] < data[mid])) idx = end;
+    else idx = end;
     return idx;
+  }
+  private static void swap(int[] data, int x, int y){
+    int temp = data[x];
+    data[x] = data[y];
+    data[y] = temp;
   }
   public static void quicksort(int[] data) {
     quicksortH(data, 0, data.length - 1);
   }
-  public static void quicksortH(int[] data, int start, int end) {
+  private static void quicksortH(int[] data, int start, int end) {
     if (start >= end) {
       return;
     }
     int pivot = partition(data, start, end);
-//    System.out.println(pivot);
-//    System.out.println(data[pivot]);
-//    System.out.println(start);
-//    System.out.println(end);
-//    System.out.println(Arrays.toString(data));
     quicksortH(data, start, pivot - 1);
-    quicksortH(data, pivot, end);
+    quicksortH(data, pivot + 1, end);
   }
   public static void main(String[] args) {
     int[] ary = {2, 10, 15, 23, 0,  5};
     int[] ary2 = {999,999,999,4,1,0,3,2,999,999,999};
     int[] ary3 = {17,61,67,47,93,12,20,4,44,78};
     int[] ary4 = {1, 2, 3, 4, 5, 6, 7, 8};
-//    quicksort(ary);
-//    System.out.println(Arrays.toString(ary));
-//    quicksort(ary2);
-//    System.out.println(Arrays.toString(ary2));
-    System.out.println(quickselect(ary, 0));
-    System.out.println(quickselect(ary, 1));
-    System.out.println(quickselect(ary, 2));
-    System.out.println(quickselect(ary, 3));
-    System.out.println(quickselect(ary, 4));
-    System.out.println(quickselect(ary, 5));
-//    quicksort(ary3);
-//    System.out.println(Arrays.toString(ary3));
-//    quicksort(ary4);
-//    System.out.println(Arrays.toString(ary4));
-//    System.out.println(findMedian(ary3, 4, 5));
-//    System.out.println(partition(ary3, 0, ary3.length - 1));
-//    System.out.println(partition(ary, 0, ary.length - 1));
+    quicksort(ary);
+    System.out.println(Arrays.toString(ary));
+    quicksort(ary2);
+    System.out.println(Arrays.toString(ary2));
+    quicksort(ary3);
+    System.out.println(Arrays.toString(ary3));
+    quicksort(ary4);
+    System.out.println(Arrays.toString(ary4));
+    System.out.println(findMedian(ary3, 4, 5));
+    System.out.println(partition(ary3, 0, ary3.length - 1));
+    System.out.println(partition(ary, 0, ary.length - 1));
+    System.out.println(quickselect(ary3, 0));
+    System.out.println(quickselect(ary3, 1));
+    System.out.println(quickselect(ary3, 2));
+    System.out.println(quickselect(ary3, 3));
+    System.out.println(quickselect(ary3, 4));
+    System.out.println(quickselect(ary3, 5));
+    System.out.println(quickselect(ary3, 6));
+    System.out.println(quickselect(ary3, 7));
+    System.out.println(quickselect(ary3, 8));
+    System.out.println(quickselect(ary3, 9));
 }
 }
