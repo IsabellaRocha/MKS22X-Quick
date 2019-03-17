9278629941227621728028719d5ee4764762c5d1
Isabella Rocha <irocha00@stuy.edu>
2019-03-17 14:07:36 -0400

    Fixed partitionDutch, implemented into quicksort

--- Quick.java
+++ Quick.java
@@ -36,7 +36,7 @@ public class Quick {
     int lo = start;
     int hi = start;
     start++;
-    while(start != end) {
+    while(start <= end) {
       if (data[start] > data[lo]) {
         swap(data, start, end);
         end--;
@@ -51,6 +51,7 @@ public class Quick {
         hi++;
       }
     }
+    //System.out.println(Arrays.toString(data));
     int[] output = {lo, hi};
     return output;
   }
@@ -58,7 +59,7 @@ public class Quick {
     int start = 0;
     int end = data.length - 1;
     int compare = partition(data, start, end);
-    while (compare != k) {
+    while (compare!=k) {
       if (compare < k) {
         start = compare + 1;
       }
@@ -91,42 +92,35 @@ public class Quick {
     if (start >= end) {
       return;
     }
-    int pivot = partition(data, start, end);
-    quicksortH(data, start, pivot - 1);
-    quicksortH(data, pivot + 1, end);
+    int pivot[] = partitionDutch(data, start, end);
+    quicksortH(data, start, pivot[0] - 1);
+    quicksortH(data, pivot[1] + 1, end);
   }
-  public static void main(String[]args){
-  System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
-  int[]MAX_LIST = {1000000000,500,10};
-  for(int MAX : MAX_LIST){
-    for(int size = 31250; size < 2000001; size*=2){
-      long qtime=0;
-      long btime=0;
-      //average of 5 sorts.
-      for(int trial = 0 ; trial <=5; trial++){
-        int []data1 = new int[size];
-        int []data2 = new int[size];
-        for(int i = 0; i < data1.length; i++){
-          data1[i] = (int)(Math.random()*MAX);
-          data2[i] = data1[i];
-        }
-        long t1,t2;
-        t1 = System.currentTimeMillis();
-        Quick.quicksort(data2);
-        t2 = System.currentTimeMillis();
-        qtime += t2 - t1;
-        t1 = System.currentTimeMillis();
-        Arrays.sort(data1);
-        t2 = System.currentTimeMillis();
-        btime+= t2 - t1;
-        if(!Arrays.equals(data1,data2)){
-          System.out.println("FAIL TO SORT!");
-          System.exit(0);
-        }
-      }
-      System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
-    }
-    System.out.println();
-  }
+  public static void main(String[] args) {
+    int[] ary = {2, 10, 15, 23, 0,  5};
+    int[] ary2 = {999,999,999,4,1,0,3,2,999,999,999};
+    int[] ary3 = {17,61,67,47,93,12,20,4,44,78};
+    int[] ary4 = {1, 2, 3, 4, 5, 6, 7, 8};
+    quicksort(ary);
+    System.out.println(Arrays.toString(ary));
+    quicksort(ary2);
+    System.out.println(Arrays.toString(ary2));
+    quicksort(ary3);
+    System.out.println(Arrays.toString(ary3));
+    quicksort(ary4);
+    System.out.println(Arrays.toString(ary4));
+//    System.out.println(findMedian(ary3, 4, 5));
+//    System.out.println(partitionDutch(ary3, 0, ary3.length - 1));
+//    System.out.println(partitionDutch(ary, 0, ary.length - 1));
+//    System.out.println(quickselect(ary3, 0));
+//    System.out.println(quickselect(ary3, 1));
+//    System.out.println(quickselect(ary3, 2));
+//    System.out.println(quickselect(ary3, 3));
+//    System.out.println(quickselect(ary3, 4));
+//    System.out.println(quickselect(ary3, 5));
+//    System.out.println(quickselect(ary3, 6));
+//    System.out.println(quickselect(ary3, 7));
+//    System.out.println(quickselect(ary3, 8));
+//    System.out.println(quickselect(ary3, 9));
 }
 }
